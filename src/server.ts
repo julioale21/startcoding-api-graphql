619ea9c2dfbd4ff349f631c427a946c41bdc8f6b
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-10-26 12:38:43 -0300

    added graphql

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import 'reflect-metadata';
+import { buildSchema } from "type-graphql";
 import { ApolloServer } from "apollo-server-express";
+import { BookResolver } from "./resolvers/book.resolver";
 
 export async function startServer() {
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [BookResolver]}),
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: '/api/graphql'})
+  
   return app;
 };
