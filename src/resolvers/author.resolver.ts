0c3c10d964506bc9ece9b19f47d44c1b859b5329
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-10-27 12:50:06 -0300

    created author resolver and added createAuthor mutation

--- src/resolvers/author.resolver.ts
+++ src/resolvers/author.resolver.ts
@@ -1,11 +1,31 @@
-import { Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import { Author } from './../entity/author.entity';
+import { getRepository, Repository } from "typeorm";
+@InputType()
+class AuthorInput {
+  @Field()
+  fullName!: string
+}
 
 @Resolver()
 export class AuthorResolver {
 
-  @Mutation(() => Author)
-  async createAuthor() {
+  authorRepository: Repository<Author>;
 
+  constructor() {
+    this.authorRepository = getRepository(Author);
+  }
+
+  @Mutation(() => Author)
+  async createAuthor(@Arg("input", () => AuthorInput ) input: AuthorInput) : Promise<Author | undefined> {
+        try {
+          const createdAuthor = await this.authorRepository.insert({
+            fullName: input.fullName
+          });
+          const result = await this.authorRepository.findOne(createdAuthor.identifiers[0].id);
+          return result;
+        } catch (error) {
+          console.error(error)
+        }
   }
 }
